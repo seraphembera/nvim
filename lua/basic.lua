889622a94fe1090a096550cf97e9edb3e329202c
seraphembera <2607068868@qq.com>
2024-01-19 20:35:06 +0800

    2023.1.19 Update

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,16 +1,18 @@
+local opt = vim.opt
+
 -- utf8
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = "utf-8"
 -- 使用相对行号
-vim.wo.number = true
-vim.wo.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 -- 显示左侧图标指示列
 vim.wo.signcolumn = "yes"
 -- Tab缩进2个空格
-vim.o.tabstop = 2
-vim.bo.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftround = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
 -- 空格代替Tab
 vim.o.expandtab = true
 vim.bo.expandtab = true
@@ -19,4 +21,4 @@ vim.o.autoindent = true
 vim.bo.autoindent = true
 vim.o.smartindent = true
 -- 配置剪切板
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
--- lua/lazynvim-init.lua
+++ lua/lazynvim-init.lua
@@ -1,7 +1,3 @@
--- 1. 准备lazy.nvim模块（存在性检测）
--- stdpath("data")
--- macOS/Linux: ~/.local/share/nvim
--- Windows: ~/AppData/Local/nvim-data
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -14,11 +10,6 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 -- 
--- 2. 将 lazypath 设置为运行时路径
--- rtp（runtime path）
--- nvim进行路径搜索的时候，除已有的路径，还会从prepend的路径中查找
--- 否则，下面 require("lazy") 是找不到的
 vim.opt.rtp:prepend(lazypath)
 
--- 3. 加载lazy.nvim模块
 require("lazy").setup("plugins")
