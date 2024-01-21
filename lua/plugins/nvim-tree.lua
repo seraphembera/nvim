1b04bb684ff8e2d1b1973fce0e1eaccf05b3f513
seraphembera <2607068868@qq.com>
2024-01-21 19:21:24 +0800

    nvim-tree bug fixes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,11 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    vim.api.nvim_create_autocmd("BufEnter", {
-      nested = true,
+    vim.api.nvim_create_autocmd("QuitPre", {
       callback = function()
-        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-          vim.cmd "quit"
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(invalid_win, w)
+          end
+        end
+        if #invalid_win == #wins - 1 then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
         end
       end
     })
