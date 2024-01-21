8e927916244e8dd2c5a083584ed28cdf161b5cc7
seraphembera <2607068868@qq.com>
2024-01-21 15:18:09 +0800

    telescope update

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -9,16 +9,10 @@ local opt = {
 
 --本地变量
 local map = vim.api.nvim_set_keymap
-
+-- 退出
 map("i", "jk", "<Esc>", opt)
-map("v", "jk", "<Esc>", opt)
 -- 粘贴
 map("i", "pp", "<Esc>pi", opt)
--- nvim-tree
-map("n", "<leader>ft", ":NvimTreeToggle<CR>", opt)
--- toggleterm
-map("n", "gg", ":ToggleTerm direction=float name=desktop<CR>i<Del>clear<CR>clear<CR>", opt)
-map("t", "jk", "<C-\\><C-n>:ToggleTerm<CR>", opt)
 -- 上下滚动浏览
 map("n", "<C-j>", "5j", opt)
 map("n", "<C-k>", "5k", opt)
@@ -39,21 +33,31 @@ map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
 map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
 map("n", "<C-Down>", ":resize +2<CR>", opt)
 map("n", "<C-Up>", ":resize -2<CR>", opt)
--- bufferline
--- 左右Tab切换
-map("n", "H", ":BufferLineCyclePrev<CR>", opt)
-map("n", "L", ":BufferLineCycleNext<CR>", opt)
 -- 关闭当前
 map("n", "sc", "<C-w>c", opt)
--- 关闭当前 buffer
-map("n", "<leader>bc", ":bdelete %<CR>", opt)
 -- 取消高亮
 map("n", "<leader>nh", ":nohl<CR>", opt)
 -- 单行或多行移动
 map("v", "J", ":m '>+1<CR>gv=gv", opt)
-map("v", "K", ":m '>-2<CR>gv=gv", opt)
+map("v", "K", ":m '<-2<CR>gv=gv", opt)
 -- 跳到行首行尾
 map("i", "<C-h>", "<ESC>I", opt)
 map("i", "<C-l>", "<ESC>A", opt)
 map("n", "<C-h>", "0", opt)
 map("n", "<C-l>", "$", opt)
+-- bufferline
+-- 左右Tab切换
+map("n", "H", ":BufferLineCyclePrev<CR>", opt)
+map("n", "L", ":BufferLineCycleNext<CR>", opt)
+-- 关闭当前 buffer
+map("n", "<leader>bc", ":bdelete %<CR>", opt)
+-- nvim-tree
+map("n", "<leader>ft", ":NvimTreeToggle<CR>", opt)
+-- toggleterm
+map("n", "gg", ":ToggleTerm direction=float name=desktop<CR>i<Del>clear<CR>clear<CR>", opt)
+map("t", "jk", "<C-\\><C-n>:ToggleTerm<CR>", opt)
+-- telescope
+map("n", "<leader>ff", [[<cmd>lua require('telescope.builtin').find_files()<cr>]], {})
+map("n", "<leader>fg", [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
+map("n", "<leader>fb", [[<cmd>lua require('telescope.builtin').buffers()<cr>]], {})
+map("n", "<leader>fh", [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], {})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
-return 
+return
 {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('telescope').setup{}
+      local actions = require("telescope.actions")
+      require('telescope').setup{
+        defaults = {
+            mappings = {
+              i = {
+                ["jk"] = actions.close
+              }
+            }
+          },
+       }
     end,
 }
