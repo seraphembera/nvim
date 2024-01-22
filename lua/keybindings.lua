8728f12837940e79e7b5134f6ddd3625b9b66dc8
seraphembera <2607068868@qq.com>
2024-01-22 15:43:06 +0800

    gx gcc

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -61,3 +61,5 @@ map("n", "<leader>ff", [[<cmd>lua require('telescope.builtin').find_files()<cr>]
 map("n", "<leader>fg", [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
 map("n", "<leader>fb", [[<cmd>lua require('telescope.builtin').buffers()<cr>]], {})
 map("n", "<leader>fh", [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], {})
+-- comment
+map("n", "gx", "gcc", opt)
