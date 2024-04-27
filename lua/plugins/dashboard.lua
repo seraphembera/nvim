fec2d1e6cda807f9c2f6a262c79cb5f00291fd8c
seraphembera <2607068868@qq.com>
2024-04-28 00:18:12 +0800

    2024.4.27 update

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,69 @@
+return
+{
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  opts = {
+      theme = "doom",
+      config = {
+          -- https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=neovim
+          header = {
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+              " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+              " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+              " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+              " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+              " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+              "                                                    ",
+          },
+          center = {
+              {
+                  icon = " ",
+                  desc = "File explorer",
+                  key = 't',
+                  action = "enew | NvimTreeToggle",
+              },
+              {
+                  icon = " ",
+                  desc = "Edit preferences   ",
+                  key = 'f',
+                  action = "Telescope find_files",
+              },
+              {
+                  icon = " ",
+                  desc = "Recent",
+                  key = 'r',
+                  action = "Telescope oldfiles",
+              },
+              {
+                  icon = " ",
+                  desc = "Settings",
+                  key = 's',
+                  action = "e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd",
+              },
+              {
+                  icon = " ",
+                  desc = "Quit",
+                  key = 'q',
+                  action = "qa",
+              },
+          },
+          footer = {},
+          },
+      },
+  config = function(_, opts)
+    require('dashboard').setup(opts)
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
